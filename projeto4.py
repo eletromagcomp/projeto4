c2585804b1050834edb2292da1f0ca498c4ea96f
bidufranjoao <49568329+bidufranjoao@users.noreply.github.com>
2019-10-21 09:12:10 -0300

    Update projeto4.py

--- projeto4.py
+++ projeto4.py
@@ -1,2 +1,21 @@
-import util4 as u4
-import pdb
+#%% Importando
+import numpy as np
+import matplotlib.pyplot as plt
+import time
+#%% Variáveis
+
+def R():
+    return 10
+
+def z_0():
+    return 2
+
+
+#%% Campo Magnético
+
+def funcao_espira(phi,x):
+    
+    a = [np.sin(phi)]
+    b = [x-R*np.cos(phi),0,z_0]
+    
+    return R*np.cross(a,b)/(np.sqrt(b.dot(b)))**3
