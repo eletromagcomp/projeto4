6c2b686aae4e7d1f2bf7f01b3d63d1b760f5ad23
Henrique Tórtura <henriquetortura.ht@gmail.com>
2019-11-10 18:19:25 -0300

    Update projeto4.py

--- projeto4.py
+++ projeto4.py
@@ -2,20 +2,64 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import time
+import sym
+
 #%% Variáveis
+def N(x=5):
+    return x
 
-def R():
-    return 10
+def L(x=1):
+    return x
 
-def z_0():
-    return 2
+def R(x=50):
+    return x
+
+#%% Integração
+
+#print(sym.simpson_malha.__doc__)
+
+#x = np.arange(0,600, 1)
+#z = np.arange(0,1000, 1)
+
+Campo = sym.simpson_malha(N(),L(),R())
+
+#%% Result
+np.savetxt('campox.dat',Campo[:,:,0])
+np.savetxt('campoy.dat',Campo[:,:,1])
+np.savetxt('campoz.dat',Campo[:,:,2])
+
+Teste = np.genfromtxt('campox.dat')
+
+print(Teste[0,0])
+
+#%% Plot
+
+fig, ax = plt.subplots()
+
+x = np.arange(-500,500)
+z = np.arange(-200,400)
+
+Campo_x = np.transpose(Campo[:,:,0])
+Campo_z = np.transpose(Campo[:,:,2])
+
+modulo = (Campo_x**2+Campo_z**2)**(1/2)
 
 
-#%% Campo Magnético
+fig, ax = plt.subplots()
+ax.streamplot(x,z,Campo_x, Campo_z, color=modulo, linewidth=2, cmap='autumn')
+plt.grid()
+#ax.set_aspect('equal')
+plt.title('How to plot a vector in matplotlib ?',fontsize=10)
+plt.savefig('how_to_plot_a_vector_in_matplotlib_fig3.png', bbox_inches='tight')
+plt.show()
+#plt.close()
 
-def funcao_espira(phi,x):
-    
-    a = [np.sin(phi)]
-    b = [x-R*np.cos(phi),0,z_0]
-    
-    return R*np.cross(a,b)/(np.sqrt(b.dot(b)))**3
+#x = np.arange(-n_malha()/2, n_malha()/2)
+#z = np.arange(-n_malha()/2, n_malha()/2)
+X, Z = np.meshgrid(x, z)
+
+fig_quiver, ax_quiver = plt.subplots()
+U = 10*Campo_z / np.sqrt(Campo_z**2 + Campo_x**2)
+V = 10*Campo_x / np.sqrt(Campo_z**2 + Campo_x**2)
+ax_quiver.quiver(X[::40,::40], Z[::40,::40], V[::40,::40], U[::40,::40], pivot='mid', color='r', units='inches')
+plt.show()
