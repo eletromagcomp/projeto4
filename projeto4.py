a8bda482ec3a9014616b3fb97fb452cd0c1959de
LordMomo <henriquetortura.ht@gmail.com>
2019-11-10 20:46:40 -0300

    plot pro campo horizontal

--- projeto4.py
+++ projeto4.py
@@ -5,22 +5,27 @@ import time
 import sym
 
 #%% Variáveis
-def N(x=1):
+def N(x=4):
     return x
 
-def L(x=10):
+def L(x=1):
     return x
 
-def R(x=5):
+def R(x=1):
     return x
 
 #%% Integração
 Campo = sym.simpson_malha(N(),L(),R())
+Campo_hor = sym.simpson_malha_hor(N(),L(),R())
 
 #%% Save Results
 np.savetxt('campox.dat',Campo[:,:,0])
 np.savetxt('campoy.dat',Campo[:,:,1])
 np.savetxt('campoz.dat',Campo[:,:,2])
+
+np.savetxt('campox_hor.dat',Campo[:,:,0])
+np.savetxt('campoy_hor.dat',Campo[:,:,1])
+np.savetxt('campoz_hor.dat',Campo[:,:,2])
 
 #Teste = np.genfromtxt('campox.dat')
 #print(Teste[0,0])
@@ -57,4 +62,31 @@ for n in range(0,limite_espiras):
     plt.plot(25, n*50/N(), 'bo')
     plt.plot(-25, n*50/N(), 'bo')
 plt.savefig('N='+str(N())+'_L='+str(L())+'_R'+str(R())+'_quiver.png', color='r', bbox_inches='tight')
+plt.show()
+
+#%% Plot
+x = np.arange(-125,125)
+y = np.arange(-125,125)
+
+X, Y = np.meshgrid(x, y)
+
+Campo_hor_x = np.transpose(Campo_hor[:,:,0])
+Campo_hor_y = np.transpose(Campo_hor[:,:,1])
+
+# if momentâneo só pro Kirby plotar os troços dele (N = 1)
+limite_espiras = 1 if N()==1 else int(N()+1)
+
+fig, ax = plt.subplots()
+fig.set_size_inches((10,10))
+strm = ax.streamplot(x, y, Campo_hor_x, Campo_hor_y, color='r', linewidth=2, cmap='autumn')
+plt.savefig('N='+str(limite_espiras)+'_L='+str(L())+'_R='+str(R())+'_streamplot_hor.png', bbox_inches='tight')
+plt.show()
+
+
+fig_quiver, ax_quiver = plt.subplots()
+fig_quiver.set_size_inches((10,10))
+U = Campo_hor_y
+V = Campo_hor_x
+ax_quiver.quiver(X[::20,::20], Y[::20,::20], V[::20,::20], U[::20,::20], units='inches')
+plt.savefig('N='+str(limite_espiras)+'_L='+str(L())+'_R='+str(R())+'_quiver_hor.png', color='r', bbox_inches='tight')
 plt.show()
