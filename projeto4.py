e1571b13a68c2ea71526222dcfac3d113b4e5f48
LordMomo <henriquetortura.ht@gmail.com>
2019-11-10 20:03:16 -0300

    Ploteeeeeeeeeee

--- projeto4.py
+++ projeto4.py
@@ -15,51 +15,46 @@ def R(x=5):
     return x
 
 #%% Integração
-
-#print(sym.simpson_malha.__doc__)
-
-#x = np.arange(0,600, 1)
-#z = np.arange(0,1000, 1)
-
 Campo = sym.simpson_malha(N(),L(),R())
 
-#%% Result
+#%% Save Results
 np.savetxt('campox.dat',Campo[:,:,0])
 np.savetxt('campoy.dat',Campo[:,:,1])
 np.savetxt('campoz.dat',Campo[:,:,2])
 
-Teste = np.genfromtxt('campox.dat')
-
-print(Teste[0,0])
+#Teste = np.genfromtxt('campox.dat')
+#print(Teste[0,0])
 
 #%% Plot
 
-fig, ax = plt.subplots()
+x = np.arange(-125,125)
+z = np.arange(-50,100)
 
-x = np.arange(-500,500)
-z = np.arange(-200,400)
+X, Z = np.meshgrid(x, z)
 
 Campo_x = np.transpose(Campo[:,:,0])
 Campo_z = np.transpose(Campo[:,:,2])
 
-modulo = (Campo_x**2+Campo_z**2)**(1/2)
-
+# if momentâneo só pro Kirby plotar os troços dele (N = 1)
+limite_espiras = 1 if N()==1 else int(N()+1)
 
 fig, ax = plt.subplots()
-ax.streamplot(x,z,Campo_x, Campo_z, color=modulo, linewidth=2, cmap='autumn')
-plt.grid()
-#ax.set_aspect('equal')
-plt.title('How to plot a vector in matplotlib ?',fontsize=10)
-plt.savefig('how_to_plot_a_vector_in_matplotlib_fig3.png', bbox_inches='tight')
+fig.set_size_inches((10,10))
+strm = ax.streamplot(x,z,Campo_x, Campo_z, color='r', linewidth=2, cmap='autumn')
+for n in range(0,limite_espiras):
+    plt.plot(25, n*50/N(), 'bo')
+    plt.plot(-25, n*50/N(), 'bo')
+plt.savefig('N='+str(N())+'_L='+str(L())+'_R='+str(R())+'_streamplot.png', bbox_inches='tight')
 plt.show()
-#plt.close()
 
-#x = np.arange(-n_malha()/2, n_malha()/2)
-#z = np.arange(-n_malha()/2, n_malha()/2)
-X, Z = np.meshgrid(x, z)
 
 fig_quiver, ax_quiver = plt.subplots()
+fig_quiver.set_size_inches((10,10))
 U = 10*Campo_z / np.sqrt(Campo_z**2 + Campo_x**2)
 V = 10*Campo_x / np.sqrt(Campo_z**2 + Campo_x**2)
-ax_quiver.quiver(X[::40,::40], Z[::40,::40], V[::40,::40], U[::40,::40], pivot='mid', color='r', units='inches')
+ax_quiver.quiver(X[::20,::20], Z[::20,::20], V[::20,::20], U[::20,::20], units='inches')
+for n in range(0,limite_espiras):
+    plt.plot(25, n*50/N(), 'bo')
+    plt.plot(-25, n*50/N(), 'bo')
+plt.savefig('N='+str(N())+'_L='+str(L())+'_R'+str(R())+'_quiver.png', color='r', bbox_inches='tight')
 plt.show()
